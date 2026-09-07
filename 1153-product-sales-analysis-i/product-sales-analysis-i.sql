ae409c5f3d71ad65d3a9f93b225439724a222262
Tarun Naidu G <welcometokannada2017@gmail.com>
2026-09-07 20:53:32 +0530

    Time: 901 ms (33.41%) | Memory: 0B (100.00%) - LeetSync

--- 1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -3,6 +3,8 @@
 select 
     p.product_name,s.year,s.price
 from 
-    Sales s,Product p 
-where 
-    s.product_id =p.product_id;
+    Sales s
+left join 
+   Product p 
+on
+    s.product_id = p.product_id;
