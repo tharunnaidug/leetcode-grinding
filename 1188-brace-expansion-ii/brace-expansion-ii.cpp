1feae73363d5f6e8bbb6356b6f8522d4a94b83d7
Tarun Naidu G <welcometokannada2017@gmail.com>
2026-09-25 09:08:46 +0530

    Time: 24 ms (5.80%) | Memory: 14 MB (89.29%) - LeetSync

---
+++ 1188-brace-expansion-ii/brace-expansion-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+    set<string> ans;
+
+    void dfs(string s){
+        int r = s.find('}');
+
+        if(r == string::npos){
+            ans.insert(s);
+            return;
+        }
+
+        int l = s.rfind('{', r);
+
+        string left = s.substr(0, l);
+        string right = s.substr(r + 1);
+
+        string inside = s.substr(l + 1, r - l - 1);
+
+        string part;
+        stringstream ss(inside);
+
+        while(getline(ss, part, ',')){
+            dfs(left + part + right);
+        }
+    }
+
+public:
+    vector<string> braceExpansionII(string expression) {
+        dfs(expression);
+        return vector<string>(ans.begin(), ans.end());
+    }
+};
