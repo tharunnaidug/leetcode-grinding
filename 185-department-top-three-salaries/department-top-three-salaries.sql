9d795154dc25b5a39fc045c96a949f9c6576a7d2
Tarun Naidu G <welcometokannada2017@gmail.com>
2026-09-25 21:27:35 +0530

    Time: 849 ms (77.23%) | Memory: 0B (100.00%) - LeetSync

---
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -0,0 +1,17 @@
+/* Write your PL/SQL query statement below */
+select
+    d.name as Department,
+    e1.name as Employee,
+    e1.salary as Salary
+from
+    Employee e1 inner join Department d
+    on e1.departmentId=d.id
+where
+    3>(
+        select 
+            count(distinct e2.salary)
+        from
+            Employee e2
+            where
+                e2.salary>e1.salary and e1.departmentId=e2.departmentId
+    )
